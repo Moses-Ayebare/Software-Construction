1e30834f01b45150ef3d39806fe53915b1f4b7b2
nkatajoshua <93196162+nkatajoshua@users.noreply.github.com>
2024-02-04 23:23:24 +0300

    Update refactoredsolid.py

--- A94161/Nkata_Joshua_A94161/SOLID Principles/refactoredsolid.py
+++ A94161/Nkata_Joshua_A94161/SOLID Principles/refactoredsolid.py
@@ -1,9 +1,12 @@
-#Create a reportwriter interface
+#Single Responsibility Principle (SRP)
 class ReportWriter:
-    def write_repor(self, employee):
+    def write_report(self, employee):
         pass
 
-#MangerReportWriter class
 class ManagerReportWriter(ReportWriter):
     def write_report(self, manager):
         print(f"Manager Report: {manager.name}")
+
+class DeveloperReportWriter(ReportWriter):
+    def write_report(self, developer):
+        print(f"Developer Report: {developer.name}")
