42569e6066d81fc219dc2023927b08119718ba17
nkatajoshua <93196162+nkatajoshua@users.noreply.github.com>
2024-02-04 23:26:39 +0300

    Update refactoredsolid.py

--- A94161/Nkata_Joshua_A94161/SOLID Principles/refactoredsolid.py
+++ A94161/Nkata_Joshua_A94161/SOLID Principles/refactoredsolid.py
@@ -10,3 +10,11 @@ class ManagerReportWriter(ReportWriter):
 class DeveloperReportWriter(ReportWriter):
     def write_report(self, developer):
         print(f"Developer Report: {developer.name}")
+
+# Open/Closed Principle (OCP)
+class ReportContext:
+    def __init__(self, report_writer):
+        self.report_writer = report_writer
+
+    def generate_report(self, employee):
+        self.report_writer.write_report(employee)
