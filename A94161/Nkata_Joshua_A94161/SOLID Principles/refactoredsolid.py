2d1b223d9ce5d10dfa06cb3d9aa49ee90212f9fc
nkatajoshua <93196162+nkatajoshua@users.noreply.github.com>
2024-02-04 23:07:58 +0300

    Update refactoredsolid.py

--- A94161/Nkata_Joshua_A94161/SOLID Principles/refactoredsolid.py
+++ A94161/Nkata_Joshua_A94161/SOLID Principles/refactoredsolid.py
@@ -2,3 +2,8 @@
 class ReportWriter:
     def write_repor(self, employee):
         pass
+
+#MangerReportWriter class
+class ManagerReportWriter(ReportWriter):
+    def write_report(self, manager):
+        print(f"Manager Report: {manager.name}")
