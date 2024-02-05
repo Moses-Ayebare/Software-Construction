86c2031407cb79da1feae9f999350fc554906b7b
AgumaDestiny <91542728+AgumaDestiny@users.noreply.github.com>
2024-02-05 23:30:55 +0300

    change two

--- A94163/SOLID WORK/refractored-solid.py
+++ A94163/SOLID WORK/refractored-solid.py
@@ -5,3 +5,9 @@ class Employee:
     def __init__(self, name, role):
         self.name = name
         self.role = role
+
+# Abstract base class outlining the protocol for crafting reports
+class ReportWriter(ABC):
+    @abstractmethod
+    def craft_report(self, employee):
+        pass
